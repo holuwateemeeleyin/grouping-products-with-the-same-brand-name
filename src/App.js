e85564a6a4293bddacc3b8fb6b38e0606efd8952
unknown <abegundeosamuel@gmail.com>
2022-03-02 21:54:11 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Products from './Component/Products'
+
+const App = () => {
+  return (
+    <div className='app_container'>
+      <Products/>
+    </div>
+  )
+}
+
+export default App
